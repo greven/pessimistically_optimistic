03d6e26042c1ec69428853ae5e6868cc7dc343a2
Nuno Freire <mail@nuno.site>
2024-06-11 17:02:58 +0100

    Set ping colors client side

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,78 +1,87 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
-
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import 'phoenix_html'
+import { Socket } from 'phoenix'
+import { LiveSocket } from 'phoenix_live_view'
+import topbar from '../vendor/topbar'
 
 // Hooks
 
 let Hooks = {}
 
 Hooks.Ping = {
-  mounted(){
+  mounted() {
     let pingEvery
+    this.valueEl = this.el.querySelector('#ping-value')
 
-    this.handleEvent("pong", () => {
+    this.handleEvent('pong', () => {
       let rtt = Date.now() - this.nowMs
       pingEvery = pingEvery ? 5000 : 1000
-      this.el.innerText = `${rtt}`
+      this.valueEl.innerText = `${rtt}`
+      this.setIndicator(rtt)
       this.timer = setTimeout(() => this.ping(rtt), pingEvery)
     })
 
     this.ping(null)
   },
 
-  reconnected(){
+  reconnected() {
     clearTimeout(this.timer)
     this.ping(null)
   },
 
-  destroyed(){ clearTimeout(this.timer) },
+  destroyed() {
+    clearTimeout(this.timer)
+  },
 
-  ping(rtt){
+  ping(rtt) {
     this.nowMs = Date.now()
-    this.pushEventTo(this.el, "ping", {rtt: rtt})
-  }
+    this.pushEventTo(this.el, 'ping', { rtt: rtt })
+  },
+
+  setIndicator(rtt) {
+    let className = 'bg-gray-300'
+
+    if (rtt < 100) {
+      className = 'bg-green-500'
+    } else if (rtt < 200) {
+      className = 'bg-yellow-500'
+    } else if (rtt < 300) {
+      className = 'bg-orange-500'
+    } else if (rtt < 500) {
+      className = 'bg-red-500'
+    } else {
+      className = 'bg-gray-300'
+    }
+
+    this.setPingClass(this.el.querySelector('#ping-indicator'), className)
+    this.setPingClass(this.el.querySelector('#ping-animation'), className)
+  },
+
+  setPingClass(el, classeName) {
+    el.classList.remove('bg-green-500', 'bg-yellow-500', 'bg-orange-500', 'bg-red-500', 'bg-gray-300')
+    el.classList.add(classeName)
+  },
 }
 
 Hooks.ListItem = {
   mounted() {
-    this.handleEvent("unhide", ({container}) => {
-      this.el.classList.remove("hidden")
+    this.handleEvent('unhide', ({ container }) => {
+      this.el.classList.remove('hidden')
       this.el.style.display = container
     })
-  }
+  },
 }
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
+let liveSocket = new LiveSocket('/live', Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-  hooks: Hooks
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' })
+window.addEventListener('phx:page-loading-start', (_info) => topbar.show(300))
+window.addEventListener('phx:page-loading-stop', (_info) => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -82,4 +91,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
--- lib/pop_web/components/live/ping_live.ex
+++ lib/pop_web/components/live/ping_live.ex
@@ -4,27 +4,21 @@ defmodule POPWeb.PingLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex justify-center items-center">
+    <div id="ping" class="flex justify-center items-center" phx-hook="Ping">
       <span class="relative flex h-2 w-2 mr-2">
         <span
-          :if={@ping}
-          class={[
-            "animate-ping absolute inline-flex h-full w-full rounded-full transition-colors opacity-75",
-            ping_class(@ping)
-          ]}
+          id="ping-animation"
+          class="animate-ping absolute inline-flex h-full w-full rounded-full transition-colors opacity-75"
         >
         </span>
-        <span class={[
-          "relative inline-flex rounded-full h-2 w-2 transition-colors",
-          ping_class(@ping)
-        ]}>
+        <span id="ping-indicator" class="relative inline-flex rounded-full h-2 w-2 transition-colors">
         </span>
       </span>
 
       <div class="flex justify-center gap-1">
         <span class="text-sm">Ping:</span>
-        <div id="ping" class="font-semibold">
-          <span id="ping-value" class="font-mono" phx-hook="Ping"></span><span class="text-zinc-600">ms</span>
+        <div class="font-semibold">
+          <span id="ping-value" class="font-mono"></span><span class="text-zinc-600">ms</span>
         </div>
       </div>
     </div>
@@ -42,15 +36,5 @@ defmodule POPWeb.PingLive do
      socket
      |> assign(ping: ping)
      |> push_event("pong", %{})}
-  end
-
-  defp ping_class(ping) do
-    cond do
-      ping < 100 -> "bg-green-500"
-      ping < 200 -> "bg-yellow-500"
-      ping < 500 -> "bg-red-500"
-      ping < 2000 -> "bg-purple-500"
-      true -> "bg-gray-300"
-    end
   end
 end
